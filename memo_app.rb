6c73df124bb01b3eb139ca44a1155f022b231b93
itagaki <itagaki_shun@vitallead.co.jp>
2025-05-29 13:51:35 +0900

    メモ新規作成画面を追加、topにタイトルを表示できるように修正

--- memo_app.rb
+++ memo_app.rb
@@ -3,12 +3,27 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
+# rubocop:disable Style/MutableConstant
+MEMO_TITLES = []
+# rubocop:enable Style/MutableConstant
+
 get '/' do
   redirect '/top'
 end
 
 get '/top' do
   @title = 'top'
-  @memo_title = '仮の実装'
+  @memo_titles = MEMO_TITLES
   erb :top_index
+end
+
+get '/new-memo' do
+  @title = 'new-memo'
+  erb :new_index
+end
+
+post '/create-memo' do
+  title = params[:title]
+  MEMO_TITLES << title unless title.nil? || title.empty?
+  redirect '/top'
 end

8f2dc0fc5e110edddfcf0b0244d9703bb1adfe2b
itagaki <itagaki_shun@vitallead.co.jp>
2025-06-06 16:42:32 +0900

    ヘルパーメソッドを導入、メモ取得処理をリファクタリング、変数名の変更

--- memo_app.rb
+++ memo_app.rb
@@ -14,13 +14,23 @@ def set_memos(file_path, memos)
   File.open(file_path, 'w') { |f| JSON.dump(memos, f) }
 end
 
+helpers do
+  def memos
+    @memos ||= get_memos(FILE_PATH)
+  end
+
+  def find_memo(id)
+    memos[id]
+  end
+end
+
 get '/' do
   redirect '/memos'
 end
 
 get '/memos' do
   @page_title = 'top'
-  @memos = get_memos(FILE_PATH)
+  @memos = memos
   erb :top_index
 end
 
@@ -33,7 +43,6 @@ post '/memos' do
   memo_title = params[:title]
   memo_content = params[:content]
 
-  memos = get_memos(FILE_PATH)
   id = ((memos.keys.map(&:to_i).max || 0) + 1).to_s
   memos[id] = { 'title' => memo_title, 'content' => memo_content }
   set_memos(FILE_PATH, memos)
@@ -44,19 +53,17 @@ end
 get '/memos/:id/edit' do
   @page_title = 'edit'
   @id = params[:id]
-  memos = get_memos(FILE_PATH)
-  @memo_title = memos[params[:id]]['title']
-  @memo_content = memos[params[:id]]['content']
+  memo = find_memo(@id)
+  @memo_title = memo['title']
+  @memo_content = memo['content']
   erb :edit_index
 end
 
 patch '/memos/:id' do
   id = params[:id]
-  memo_title = params[:title]
-  memo_content = params[:content]
-
-  memos = get_memos(FILE_PATH)
-  memos[id] = { 'title' => memo_title, 'content' => memo_content }
+  memo = find_memo(id)
+  memo['title'] = params[:title]
+  memo['content'] = params[:content]
   set_memos(FILE_PATH, memos)
 
   redirect '/memos'
@@ -65,15 +72,15 @@ end
 get '/memos/:id' do
   @page_title = 'show'
   @id = params[:id]
-  memos = get_memos(FILE_PATH)
-  @memo_title = memos[params[:id]]['title']
-  @memo_content = memos[params[:id]]['content']
+  memo = find_memo(@id)
+  @memo_title = memo['title']
+  @memo_content = memo['content']
   erb :show_index
 end
 
 delete '/memos/:id' do
-  memos = get_memos(FILE_PATH)
-  memos.delete(params[:id])
+  id = params[:id]
+  memos.delete(id)
   set_memos(FILE_PATH, memos)
 
   redirect '/memos'
